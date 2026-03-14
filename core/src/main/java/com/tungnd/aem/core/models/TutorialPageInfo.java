3e65d49fad3916a3d24b1b536c46e3f0de7d0c67
tungndse <tungndse@gmail.com>
2026-03-14 22:13:36 +0700

    Improve tutorial banner authoring and presentation

--- core/src/main/java/com/tungnd/aem/core/models/TutorialPageInfo.java
+++ core/src/main/java/com/tungnd/aem/core/models/TutorialPageInfo.java
@@ -1,4 +1,15 @@
 package com.tungnd.aem.core.models;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 public class TutorialPageInfo {
+
+
+
 }
