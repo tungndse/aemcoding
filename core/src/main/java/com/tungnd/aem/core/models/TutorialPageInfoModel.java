1291a8b8bdd39c5af7164a500e3fe54b1a165485
tungndse <tungndse@gmail.com>
2026-03-15 21:52:38 +0700

    Add tutorial page template configuration

--- core/src/main/java/com/tungnd/aem/core/models/TutorialPageInfoModel.java
+++ core/src/main/java/com/tungnd/aem/core/models/TutorialPageInfoModel.java
@@ -1,6 +1,8 @@
 package com.tungnd.aem.core.models;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
@@ -16,11 +18,11 @@ public class TutorialPageInfoModel {
     @Self
     private Resource resource;
 
-    @ScriptVariable
-    private Page currentPage;
-
     @ValueMapValue
     private String theme;
+
+    /*@ScriptVariable
+    private Page currentPage;
 
     public String getPagePath() {
         return currentPage != null ? currentPage.getPath() : "";
@@ -48,6 +50,50 @@ public class TutorialPageInfoModel {
         if (currentPage != null && currentPage.getParent() != null)
             return currentPage.getParent().getPath();
 
+        return "";
+    }*/
+
+    private Page getCurrentPage() {
+        if (resource == null) {
+            return null;
+        }
+
+        ResourceResolver resolver = resource.getResourceResolver();
+        PageManager pageManager = resolver.adaptTo(PageManager.class);
+
+        return pageManager != null ? pageManager.getContainingPage(resource) : null;
+    }
+
+    public String getPagePath() {
+        Page currentPage = getCurrentPage();
+        return currentPage != null ? currentPage.getPath() : "";
+    }
+
+    public String getPageTitle() {
+        Page currentPage = getCurrentPage();
+        return currentPage != null ? currentPage.getTitle() : "";
+    }
+
+    public String getResourcePath() {
+        return resource != null ? resource.getPath() : "";
+    }
+
+    public String getResourceType() {
+        return resource != null ? resource.getResourceType() : "";
+    }
+
+    public String getTemplatePath() {
+        Page currentPage = getCurrentPage();
+        return currentPage != null
+                ? currentPage.getProperties().get("cq:template", "")
+                : "";
+    }
+
+    public String getParentPath() {
+        Page currentPage = getCurrentPage();
+        if (currentPage != null && currentPage.getParent() != null) {
+            return currentPage.getParent().getPath();
+        }
         return "";
     }
 
